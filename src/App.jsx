00061ecbdb09e69b1e1c76f2269dea37278644c7
John Divine Mathew <mathewdivine95@gmail.com>
2026-06-02 12:38:03 +0530

    navbar.jsx and home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center mt-10">
-        Hire-Radar
-      </h1>
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
