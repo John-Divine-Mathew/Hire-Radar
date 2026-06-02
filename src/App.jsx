b64a7190c2c8a209d6e01a47c0c9091dbb363f8f
Vijayanandha J <vijayanandhaj@gmail.com>
2026-06-02 11:16:07 +0530

    Trying git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">
+        Hire-Radar
+      </h1>
+      <h1>HIROTEC</h1>
+    </div>
+  )
+}
+
+export default App
