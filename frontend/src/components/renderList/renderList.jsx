7f8f4fcb20a1513e1614922c214369e1d7d8a1c3
Vijayanandha J <vijayanandhaj@gmail.com>
2026-06-11 11:07:54 +0530

    minor changes in renderlist.jsx

--- frontend/src/components/renderList/renderList.jsx
+++ frontend/src/components/renderList/renderList.jsx
@@ -1,35 +1,42 @@
-import { User } from 'lucide-react';
+import { Bookmark, User } from 'lucide-react';
 import PropTypes from "prop-types";
 import './renderList.css';
 
 function RenderList(props){
-
-    const text = props.skills
-    const skillsArr = text.split(",");
+    const cndArray = props.Array;
 
     return(
-            <li className='renderListItem'>
-                <div className='profileDiv'>
-                    <div className='cndIcon'><User size={40}/></div>
-                    <div className='cndDetails'> 
-                        <p className='nameP'>{props.name}</p>
-                        <ul className='detailsList'>
-                            <li className='detailsListItem'>{props.role}</li>
-                            <li className='detailsListItem'>{props.experience}</li>
-                            <li className='detailsListItem'>{props.location}</li>
-                            <li className='detailsListItem'>{props.status}</li>
-                        </ul>
-                        <ul className='skillsList'>
-                            {skillsArr.map((s)=>(<li className='skillListItem'>{s}</li>))}
+        <div className='renderListDiv'><ul>
+
+            {cndArray.map((i)=>(
+                <li className='renderListItem'>
+                    <div className='profileDiv'>
+                        <div className='cndIcon'><User size={40}/></div>
+                        <div className='cndDetails'> 
+                            <p className='nameP'>{i.cndname}</p>
+                            <ul className='detailsList'>
+                                <li className='detailsListItem'>{i.cndrole}</li>
+                                <li className='detailsListItem'>{i.cndexperience}</li>
+                                <li className='detailsListItem'>{i.cndlocation}</li>
+                                <li className='detailsListItem'>{i.cndstatus}</li>
+                            </ul>
+                            <ul className='skillsList'>
+                                {i.cndskills.split(',').map((s)=>(<li className='skillListItem'>{s}</li>))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='profileEndDiv'>
+                        <Bookmark size='40' className='saveButton'/>
+                        <ul className='cndScoreView'>
+                            <li>Match Score</li>
+                            <li>80%</li>
+                            <li><button className='profileButton'>View Profile</button></li>
                         </ul>
                     </div>
-                </div>
-                <ul className='cndScoreView'>
-                    <li>Match Score</li>
-                    <li>80%</li>
-                    <li><button className='profileButton'>View Profile</button></li>
-                </ul>
-            </li>
+                </li>
+            ))}
+
+        </ul></div>
     );
 
 }
