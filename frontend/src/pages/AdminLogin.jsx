46927ef15e4fa806cc61063f577703e9b6506c6c
bharath snehan <bharathjeeva549@gmail.com>
2026-06-19 11:31:00 +0530

    Dashboard Changes

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AdminLogin = () => {
-
   const navigate = useNavigate();
-    
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -13,55 +12,59 @@ const AdminLogin = () => {
     "mathewdivine95@gmail.com",
     "bharathjeeva549@gmail.com",
     "vijayanandhaj@gmail.com",
-    "test@gmail.com"
+    "test@gmail.com",
   ];
 
   const handleLogin = (e) => {
-
     e.preventDefault();
 
     if (
       teamMembers.includes(email) &&
       password === "Hirotec@123"
     ) {
+      const userData = {
+        name: name,
+        email: email,
+      };
+
+      localStorage.setItem(
+        "user",
+        JSON.stringify(userData)
+      );
 
       alert("Login Successful");
 
       navigate("/dashboard");
-
     } else {
-
       alert("Invalid Email or Password");
-
     }
-
   };
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
-
       <div className="bg-white shadow-lg rounded-xl p-8 w-[400px]">
-
         <h1 className="text-3xl font-bold text-center text-purple-700 mb-6">
           HR / Admin Login
         </h1>
 
         <form onSubmit={handleLogin}>
 
-            <input 
+          <input
             type="text"
             placeholder="Enter Name"
             className="w-full border p-3 rounded mb-4"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            />  
-    
+            required
+          />
+
           <input
             type="email"
             placeholder="Enter Email"
             className="w-full border p-3 rounded mb-4"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
 
           <input
@@ -70,6 +73,7 @@ const AdminLogin = () => {
             className="w-full border p-3 rounded mb-4"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
 
           <button
@@ -80,9 +84,7 @@ const AdminLogin = () => {
           </button>
 
         </form>
-
       </div>
-
     </div>
   );
 };
