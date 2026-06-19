ffae9c5d3566e45a2e5549b7d2ceccccc3645264
bharath snehan <bharathjeeva549@gmail.com>
2026-06-19 11:31:00 +0530

    Dashboard Changes

--- frontend/src/pages/AdminUser.jsx
+++ frontend/src/pages/AdminUser.jsx
@@ -2,50 +2,52 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AdminLogin = () => {
-
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const teamMembers = [
-    "test@gmail.com",
-    "mathewdivine95@gmail.com"
-    
+    {
+      email: "test@gmail.com",
+      name: "Bharath",
+    },
+    {
+      email: "mathewdivine95@gmail.com",
+      name: "Mathew",
+    },
+    {
+      email: "admin@gmail.com",
+      name: "Admin",
+    },
   ];
 
   const handleLogin = (e) => {
-
     e.preventDefault();
 
-    if (
-      teamMembers.includes(email) &&
-      password === "Hirotec@123"
-    ) {
+    const user = teamMembers.find(
+      (member) => member.email === email
+    );
 
-      alert("Login Successful");
+    if (user && password === "Hirotec@123") {
+      localStorage.setItem("adminName", user.name);
+
+      alert(`Welcome ${user.name}`);
 
       navigate("/assessmentform");
-
     } else {
-
       alert("Invalid Email or Password");
-
     }
-
   };
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
-
       <div className="bg-white shadow-lg rounded-xl p-8 w-[400px]">
-
         <h1 className="text-3xl font-bold text-center text-purple-700 mb-6">
           User Login
         </h1>
 
         <form onSubmit={handleLogin}>
-
           <input
             type="email"
             placeholder="Enter Email"
@@ -68,11 +70,8 @@ const AdminLogin = () => {
           >
             Login
           </button>
-
         </form>
-
       </div>
-
     </div>
   );
 };
